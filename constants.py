5915ccbd39856153a27f6ac31c4b28187221a2b6
aleksandrasz01 <szymula.aleksandra01@gmail.com>
2025-12-22 23:05:11 -0800

    Add constants for grid dimensions and colors

---
+++ constants.py
@@ -0,0 +1,16 @@
+WIDTH = 800
+HEIGHT = 800
+ROWS = 40
+COLS = 40
+CELL_SIZE = WIDTH // COLS
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+
+GREEN = (0, 255, 0)     # Start
+RED = (255, 0, 0)       # End
+BLUE = (0, 0, 255)      # Frontier
+YELLOW = (255, 255, 0)  # Path
+PURPLE = (160, 32, 240) # Visited
